ca3457b253e40b27e4bd25495d470feb5264f87f
Gideon0123 <nwokporochijiokegideon@gmail.com>
2026-07-29 10:20:03 +0100

    RabbitMQ Configurations

--- REVIEW-SERVICE/src/main/java/com/example/REVIEW_SERVICE/publisher/ReviewEventPublisherImpl.java
+++ REVIEW-SERVICE/src/main/java/com/example/REVIEW_SERVICE/publisher/ReviewEventPublisherImpl.java
@@ -4,90 +4,92 @@ import com.example.REVIEW_SERVICE.dto.events.*;
 import com.example.REVIEW_SERVICE.utils.RabbitMQConstants;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
-@Service
+@Component
 @RequiredArgsConstructor
-public class RabbitReviewEventPublisher implements ReviewEventPublisher {
+public class ReviewEventPublisherImpl implements ReviewEventPublisher {
 
     private final RabbitTemplate rabbitTemplate;
+
+    private void publish(
+            String routingKey,
+            Object event
+    ) {
+
+        rabbitTemplate.convertAndSend(
+                RabbitMQConstants.REVIEW_EXCHANGE,
+                routingKey,
+                event
+        );
+
+    }
 
     @Override
     public void publishAssigned(
             ReviewAssignedEvent event
     ) {
-        rabbitTemplate.convertAndSend(
-                RabbitMQConstants.REVIEW_EXCHANGE,
+        publish(
                 RabbitMQConstants.REVIEW_ASSIGNMENT_ROUTING_KEY,
                 event
         );
-
     }
 
     @Override
     public void publishAccepted(
             ReviewAcceptedEvent event
     ) {
-        rabbitTemplate.convertAndSend(
-                RabbitMQConstants.REVIEW_EXCHANGE,
+        publish(
                 RabbitMQConstants.REVIEW_ACCEPTED_ROUTING_KEY,
                 event
         );
-
     }
 
     @Override
     public void publishDeclined(
             ReviewDeclinedEvent event
     ) {
-        rabbitTemplate.convertAndSend(
-                RabbitMQConstants.REVIEW_EXCHANGE,
+        publish(
                 RabbitMQConstants.REVIEW_DECLINED_ROUTING_KEY,
                 event
         );
-
     }
 
     @Override
     public void publishSubmitted(
             ReviewSubmittedEvent event
     ) {
-        rabbitTemplate.convertAndSend(
-                RabbitMQConstants.REVIEW_EXCHANGE,
+        publish(
                 RabbitMQConstants.REVIEW_SUBMITTED_ROUTING_KEY,
                 event
         );
-
     }
 
     @Override
     public void publishDecision(
             EditorialDecisionEvent event
     ) {
-        rabbitTemplate.convertAndSend(
-                RabbitMQConstants.REVIEW_EXCHANGE,
+        publish(
                 RabbitMQConstants.REVIEW_DECISION_ROUTING_KEY,
                 event
         );
-
     }
 
     @Override
     public void publishRevisionRequested(
             RevisionRequestedEvent event
     ) {
-        rabbitTemplate.convertAndSend(
-                RabbitMQConstants.REVIEW_EXCHANGE,
+        publish(
                 RabbitMQConstants.REVIEW_REVISION_ROUTING_KEY,
                 event
         );
-
     }
 
     @Override
-    public void publishReviewReminder(ReviewReminderEvent event) {
-        rabbitTemplate.convertAndSend(
-                RabbitMQConstants.REVIEW_EXCHANGE,
+    public void publishReviewReminder(
+            ReviewReminderEvent event
+    ) {
+        publish(
                 RabbitMQConstants.REVIEW_REMINDER_ROUTING_KEY,
                 event
         );
